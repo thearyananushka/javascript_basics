998bfe93a2df09fe056dbddc7b9d5412489e2850
Anushka Aryan <thearyananushka@gmail.com>
2024-07-26 16:31:49 +0530

    Initial commit

---
+++ ep/index.js
@@ -0,0 +1,16 @@
+// template literals 
+ const age = 21 ;
+ const fullName = "John Doe";
+
+ //output : Hello Mr John Doe your age is : 21
+
+ console.log("Hello Mr"+ fullName + " your age is " + age);
+
+ console.log(`Hello Mr ${fullName} your age is: ${age} `);
+
+ // print sum of two numbers
+ const a= 10;
+ const b = 20;
+ console.log ("sum of" +a+ "and" +b+ "is:"  +a+b)
+ console.log (`sum of ${a} and ${b} is ${a+b}`)
+ console.log (`sum of ${a} and ${b} is ${Math.random(10)}`)
